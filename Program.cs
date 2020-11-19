30fdb9487a24893142fdf19885f0006c5a49800c
Lacey Walker <bluelarksong@gmail.com>
2020-11-19 12:03:46 -0600

    attempting to sort fruit by the letter L, saving progress

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace linq2
 {
@@ -7,9 +9,23 @@ namespace linq2
         static void Main(string[] args)
         {
             // Find the words in the collection that start with the letter 'L'
-            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+            List<string> fruits = new List<string>()
+            {
+                "Lemon",
+                "Apple",
+                "Orange",
+                "Lime",
+                "Watermelon",
+                "Loganberry"
 
-            IEnumerable<string> LFruits = from fruit in fruits where
+            };
+
+            IEnumerable<string> LFruits = from fruit in fruits.Where(fruits => fruits.StartsWith("L"))
+
+            Console.WriteLine(LFruits);
+
+            // where fruit.StartsWith(L)
+            // select fruit;
         }
-    }
-}
+    };
+};
