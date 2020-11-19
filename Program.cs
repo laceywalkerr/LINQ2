a78bfafe2d1058bbe5d4d0dcf97bf94cb7732038
Lacey Walker <bluelarksong@gmail.com>
2020-11-19 13:59:33 -0600

    listing numbers in order

--- Program.cs
+++ Program.cs
@@ -27,6 +27,66 @@ namespace linq2
             //     Console.WriteLine(fruit);
             // }
 
+            // List<int> numbers = new List<int>()
+            // {
+            //     15,
+            //     8,
+            //     21,
+            //     24,
+            //     32,
+            //     13,
+            //     30,
+            //     12,
+            //     7,
+            //     54,
+            //     48,
+            //     4,
+            //     49,
+            //     96
+            // };
+
+            // IEnumerable<int> fourSixMultiples = numbers.Where(number => number % 4 == 0 || number % 6 == 0);
+            // foreach (int number in fourSixMultiples)
+            // {
+            //     Console.WriteLine(number);
+            // }
+
+            // Order these student names alphabetically, in descending order (Z to A)
+            // List<string> names = new List<string>()
+            // {
+            //     "Heather",
+            //     "James",
+            //     "Xavier",
+            //     "Michelle",
+            //     "Brian",
+            //     "Nina",
+            //     "Kathleen",
+            //     "Sophia",
+            //     "Amir",
+            //     "Douglas",
+            //     "Zarley",
+            //     "Beatrice",
+            //     "Theodora",
+            //     "William",
+            //     "Svetlana",
+            //     "Charisse",
+            //     "Yolanda",
+            //     "Gregorio",
+            //     "Jean-Paul",
+            //     "Evangelina",
+            //     "Viktor",
+            //     "Jacqueline",
+            //     "Francisco",
+            //     "Tre"
+            // };
+
+            // IEnumerable<string> descend = names.OrderByDescending(n => n);
+            // foreach (string name in descend)
+            // {
+            //     Console.WriteLine(name);
+            // }
+
+            // Build a collection of these numbers sorted in ascending order
             List<int> numbers = new List<int>()
             {
                 15,
@@ -45,45 +105,10 @@ namespace linq2
                 96
             };
 
-            IEnumerable<int> fourSixMultiples = numbers.Where(number => number % 4 == 0 || number % 6 == 0);
-            foreach (int number in fourSixMultiples)
+            IEnumerable<int> ascend = numbers.OrderBy(n => n);
+            foreach (int number in ascend)
             {
                 Console.WriteLine(number);
-            }
-
-            // Order these student names alphabetically, in descending order (Z to A)
-            List<string> names = new List<string>()
-            {
-                "Heather",
-                "James",
-                "Xavier",
-                "Michelle",
-                "Brian",
-                "Nina",
-                "Kathleen",
-                "Sophia",
-                "Amir",
-                "Douglas",
-                "Zarley",
-                "Beatrice",
-                "Theodora",
-                "William",
-                "Svetlana",
-                "Charisse",
-                "Yolanda",
-                "Gregorio",
-                "Jean-Paul",
-                "Evangelina",
-                "Viktor",
-                "Jacqueline",
-                "Francisco",
-                "Tre"
-            };
-
-            IEnumerable<string> descend = names.OrderByDescending(n => n);
-            foreach (string name in descend)
-            {
-                Console.WriteLine(name);
             }
 
         }
